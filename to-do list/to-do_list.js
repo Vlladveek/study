a0dfeae44c7db6288c1149a3c352b595041f563b
Vlladveek <vlladveek@gmail.com>
2026-04-05 11:16:09 +0300

    homework

--- to-do list/to-do_list.js
+++ to-do list/to-do_list.js
@@ -1,27 +1,34 @@
-const addButton = document.querySelector(".addButton")
-addButton.addEventListener("click", () => {
-    //let checkBox = document.createElement("checkbox")
-    //li.appendChild(checkBox)
-    
-    /*let checkBox = document.createElement("input")
-    checkBox.type = "checkbox"
-    li.prepend(checkBox)*/ 
+function main() {
+    //Array.from()
+    const addButton = document.querySelector(".addButton")
+    addButton.addEventListener("click", () => {
+        //let checkBox = document.createElement("checkbox")
+        //li.appendChild(checkBox)
 
-    let input = document.querySelector(".inputText")
-    let textFromInput = input.value
-    let ul = document.querySelector(".ul")
-    let li = document.createElement("li")
-    li.textContent = textFromInput
-    ul.appendChild(li)
+        /*let checkBox = document.createElement("input")
+        checkBox.type = "checkbox"
+        li.prepend(checkBox)*/
 
-    let button = document.createElement("button")
-    button.addEventListener("click", () => {
-        li.remove()
+        let input = document.querySelector(".inputText")
+        let textFromInput = input.value
+        let ul = document.querySelector(".ul")
+        let li = document.createElement("li")
+        li.classList.add("list")
+        li.textContent = textFromInput
+        ul.appendChild(li)
+
+        let button = document.createElement("button")
+        button.classList.add("delete-btn")
+        button.addEventListener("click", () => {
+            li.remove()
+        })
+        button.textContent = "Удалить"
+        li.appendChild(button)
+
+        let checkBox = document.createElement("input")
+        checkBox.type = "checkbox"
+        checkBox.classList.add("checkbox")
+        li.prepend(checkBox)
     })
-    button.textContent = "Удалить"
-    li.appendChild(button)
-
-    let checkBox = document.createElement("input")
-    checkBox.type = "checkbox"
-    li.prepend(checkBox)
-})
+}
+document.addEventListener("DOMContentLoaded", main)
